6c2e8c0639590f384e8c54a4637300934c376b92
Areg Harutyunyan <harutyunyanareg2011@gmail.com>
2024-06-29 21:41:08 -0600

    fix publish command

--- src/main/kotlin/commands/PublishCommand.kt
+++ src/main/kotlin/commands/PublishCommand.kt
@@ -2,25 +2,8 @@ package com.mycelium.commands
 
 import com.mycelium.arguments.Gamemode
 
-class PublishCommand() : BaseCommand("publish") {
-    private var allowCommands: Boolean? = null
-    private var gamemode: Gamemode? = null
-    private var port: Int? = null // docs say this should be an Int, but ports are technically UShorts
-
-    fun allowCommands(allow: Boolean): PublishCommand {
-        allowCommands = allow
-        return this
-    }
-
-    fun gamemode(mode: Gamemode): PublishCommand {
-        gamemode = mode
-        return this
-    }
-
-    fun port(port: Int): PublishCommand {
-        this.port = port
-        return this
-    }
+class PublishCommand(private val allowCommands: Boolean? = null, private val gamemode: Gamemode? = null,
+                     private val port: Int? = null /* docs say this should be an Int, but ports are technically UShorts */) : BaseCommand("publish") {
 
     override fun toMinecraft(): String {
         allowCommands?.toString()?.let{ addArgument(it) }
