ec9857ce3c925a570318a888705b410cb6f38efe
Areg Harutyunyan <harutyunyanareg2011@gmail.com>
2024-06-29 20:28:48 -0600

    add tick command

---
+++ src/main/kotlin/commands/TickCommand.kt
@@ -0,0 +1,43 @@
+package com.mycelium.commands
+
+class TickCommand() : BaseCommand("tick") {
+    fun query(): CommandEnd {
+        return CommandEnd(this, "query")
+    }
+
+    fun rate(rate: Float): CommandEnd {
+        return CommandEnd(this, "rate", rate.toString())
+    }
+
+    fun freeze(): CommandEnd {
+        return CommandEnd(this, "freeze")
+    }
+
+    fun unfreeze(): CommandEnd {
+        return CommandEnd(this, "unfreeze")
+    }
+
+    fun step(): CommandEnd {
+        return CommandEnd(this, "step")
+    }
+
+    fun step(time: Float): CommandEnd {
+        return CommandEnd(this, "step", time.toString())
+    }
+
+    fun stepStop() : CommandEnd {
+        return CommandEnd(this, "step", "stop")
+    }
+
+    fun sprint(time: Float): CommandEnd {
+        return CommandEnd(this, "sprint", time.toString())
+    }
+
+    fun sprintStop(): CommandEnd {
+        return CommandEnd(this, "sprint", "stop")
+    }
+
+    override fun toMinecraft(): String {
+        return super.toMinecraft()
+    }
+}
