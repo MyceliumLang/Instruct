43a314eb4cc7b1ee842123562d1c9cad6fea91ab
Areg Harutyunyan <harutyunyanareg2011@gmail.com>
2024-06-29 20:54:06 -0600

    perf command

---
+++ src/main/kotlin/commands/PerfCommand.kt
@@ -0,0 +1,15 @@
+package com.mycelium.commands
+
+class PerfCommand() : BaseCommand("perf") {
+    fun start(): CommandEnd {
+        return CommandEnd(this, "start")
+    }
+
+    fun stop(): CommandEnd {
+        return CommandEnd(this, "stop")
+    }
+
+    override fun toMinecraft(): String {
+        return super.toMinecraft()
+    }
+}
