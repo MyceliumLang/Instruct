e9ac88d8ec28b90210f1e7c013c4fbb13fac4ed9
Areg Harutyunyan <harutyunyanareg2011@gmail.com>
2024-06-29 21:22:48 -0600

    datapack command

---
+++ src/main/kotlin/commands/DatapackCommand.kt
@@ -0,0 +1,56 @@
+package com.mycelium.commands
+
+import com.mycelium.Minecraftable
+
+enum class DatapackPriority(private val mcName: String) : Minecraftable {
+    First("first"),
+    Last("last");
+
+    override fun toMinecraft(): String {
+        return mcName
+    }
+}
+
+enum class RelativeDatapackPriority(private val mcName: String) : Minecraftable {
+    Before("before"),
+    After("after");
+
+    override fun toMinecraft(): String {
+        return mcName
+    }
+}
+
+enum class DatapackType(private val mcName: String) : Minecraftable {
+    Available("available"),
+    Enabled("enabled");
+
+    override fun toMinecraft(): String {
+        return mcName
+    }
+}
+
+class DatapackCommand() : BaseCommand("datapack") {
+    fun disable(name: String): CommandEnd {
+        return CommandEnd(this, "disable", name)
+    }
+
+    fun enable(name: String): CommandEnd {
+        return CommandEnd(this, "enable", name)
+    }
+
+    fun enable(name: String, priority: DatapackPriority): CommandEnd {
+        return CommandEnd(this, "enable", name, priority.toMinecraft())
+    }
+
+    fun enable(name: String, relativePriority: RelativeDatapackPriority, existing: String): CommandEnd {
+        return CommandEnd(this, "enable", name, relativePriority.toMinecraft(), existing)
+    }
+
+    fun list(type: DatapackType): CommandEnd {
+        return CommandEnd(this, "list", type.toMinecraft())
+    }
+
+    override fun toMinecraft(): String {
+        return super.toMinecraft()
+    }
+}
